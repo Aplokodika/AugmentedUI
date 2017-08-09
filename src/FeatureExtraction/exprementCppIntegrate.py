cc80aa8f4963f85e58ee796518398cb14c3b513e
Sreram K <sreramk@outlook.com>
2017-08-10 03:11:48 +0530

    Successfully
    This source contains the library in '.so' format. This dynamic library was compiled in Ubuntu 16.04 LTS.

---
+++ src/FeatureExtraction/exprementCppIntegrate.py
@@ -0,0 +1,28 @@
+'''
+Created on 07-Aug-2017
+
+@author: sreram
+'''
+
+"""
+import ImageProcessExtension
+
+
+
+
+
+
+a = ImageProcessExtension.World ()
+
+a.set("hello world")
+
+a.greet()
+
+x = (1,2,3,4,5,6,7,8,9,10)
+
+a.helloWorld()
+
+x = a.displayNumbers(x)
+a.displayNumbers(x)
+
+"""
