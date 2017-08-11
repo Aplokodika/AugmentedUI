56445042109e0ef416d5ef9092a2552eba70749f
Sreram K <sreramk@outlook.com>
2017-08-12 00:16:19 +0530

    Contains untested source code.
    Must test the source added to these files.

--- src_cpp_dependence/imageprocutil.cpp
+++ src_cpp_dependence/imageprocutil.cpp
@@ -21,6 +21,9 @@
 #include <vector>
 #include <stdint.h>
 #include <deque>
+#include <fstream>
+#include <assert.h>
+#include <string>
 
 py::dict globals;
 
@@ -80,16 +83,175 @@ struct enable_numpy_scalar_converter
 
 const uint64_t NO_OF_COLORS_PER_PIXEL = 3;
 
+const int ImageProcessExtension::_READ_   = 0;
+const int ImageProcessExtension::_WRITE_  = 1;
+const int ImageProcessExtension::_APPEND_ = 2;
 
-py::list ImageProcessExtension::exaggerateColorByOrder(py::object& pImageFrame, py::object porder) {
+const int ImageProcessExtension::_DYNAMIC_BIND_ = 0;
+const int ImageProcessExtension::_INSTANT_BIND  = 1;
+
+const int ImageProcessExtension::_MAX_THRESHOLD_DYNAMIC_ = 1000;
+
+
+const std::string ImageProcessExtension::__one   	(",1");
+const std::string ImageProcessExtension::__zero  	(",0");
+const std::string ImageProcessExtension::__comma 	(",");
+const std::string ImageProcessExtension::__nextline ("\n");
+
+
+ImageProcessExtension::ImageProcessExtension ():
+								openMode (-1),
+								bindType(_DYNAMIC_BIND_),
+								fHandle (NULL){
+
+}
+
+ImageProcessExtension::~ImageProcessExtension() {
+	close_connection();
+}
+
+
+void ImageProcessExtension::close_connection () {
+	if (fHandle != NULL) {
+		fHandle->close();
+		delete fHandle;
+	}
+	fHandle = NULL;
+}
+
+py::list ImageProcessExtension::convert_image_frame_to_list (py::object& pImageFrame) {
+
+	globals["imageFrame"] = pImageFrame;
+	py::exec("imageFrame = list(imageFrame)", globals);
+	return py::list(globals["imageFrame"]);
+}
+
+
+std::string ImageProcessExtension::itoa(int num) {
+
+	char* buffer = new char [12];
+	sprintf(buffer,"%d", num);
+	std::string retval(buffer);
+	delete [] buffer;
+	return retval;
+}
+
+
+void ImageProcessExtension::force_flush_file_buffer () {
+
+	if ( openMode != _APPEND_ || bindType != _DYNAMIC_BIND_)
+		return;
+
+	while (fileBuffer.size()) {
+		(*fHandle) << fileBuffer[0];
+		fileBuffer.pop_front(); // acts like a queue. Pushed back, popped front.
+	}
+}
+
+
+void ImageProcessExtension::dynamicBindData() {
+
+	if (fileBuffer.size() < _MAX_THRESHOLD_DYNAMIC_)
+		return;
+
+	force_flush_file_buffer ();
+}
+
+void ImageProcessExtension::write_binary_image_clip_to_CSV_file (py::object& pImageFrame) {
+
+	assert ( (openMode == _WRITE_ ||
+			  openMode == _APPEND_ ) &&
+			"Error, the file should have been opened in WRITE or APPEND mode before calling this method");
+
+	py::list imageFrame  = convert_image_frame_to_list (pImageFrame);
+
+	int ySize = py::len(imageFrame);
+	int xSize = py::len(imageFrame[0]);
+
+	std::string lineToBeAdded;
+
+	lineToBeAdded = std::string(itoa(ySize)) + __comma +
+			 	 	std::string(itoa(xSize));
+
+	for (int i = 0; i < ySize; i++) {
+		for (int j = 0; j < xSize; j++) {
+
+			uint8_t temp = py::extract<uint8_t>(imageFrame[i][j][0]);
+
+			assert (((temp == py::extract<uint8_t>(imageFrame[i][j][1])) &&
+					(temp ==py::extract<uint8_t>(imageFrame[i][j][2]) )) &&
+					(temp == 255 || temp == 0 || temp == 1)	&&
+					 "Error, this method deals only with binary images. You have given a non-binary image");
+
+			if (temp == 0) {
+				lineToBeAdded = lineToBeAdded + __zero;
+			} else {
+				lineToBeAdded = lineToBeAdded + __one;
+			}
+
+		}
+	}
+
+	lineToBeAdded = lineToBeAdded + __nextline;
+
+	assert ( (bindType == _DYNAMIC_BIND_  ||
+				  bindType == _INSTANT_BIND)  &&
+				 "Error, invalid bindType encountered");
+
+	if (bindType == _DYNAMIC_BIND_) {
+		fileBuffer.push_back(lineToBeAdded);
+		dynamicBindData();
+	} else {
+		assert (openMode == _APPEND_ && "Error, the open mode must be APPEND, if INSTANT_BIND");
+		(*fHandle) << lineToBeAdded;
+		fHandle->flush();
+	}
+}
+
+int ImageProcessExtension::exists(const char *fname)
+{
+    FILE *file;
+    if ((file = fopen(fname, "r")))
+    {
+        fclose(file);
+        return 1;
+    }
+    return 0;
+}
+
+void ImageProcessExtension::connect_to_file (py::object& pFName,
+		py::object& pOpenMode, py::object& pBindType) {
+
+	force_flush_file_buffer();
+	close_connection();
+
+	std::string fName  = py::extract<std::string> (pFName);
+	openMode = py::extract <int> (pOpenMode);
+	bindType = py::extract <int> (pBindType);
+
+	assert ( (openMode == _READ_   ||
+			  openMode == _WRITE_  ||
+			  openMode == _APPEND_  ) && "Error, pOpenMode can only take the values 0(READ), 1(WRITE) or 2(APPEND)");
+
+	std::ios_base::openmode opMode =  (openMode == _READ_)  ? std::ios_base::in:
+			  	  	  	   ((openMode == _WRITE_) ? std::ios_base::out:
+			  	  	  			   	   	   	   	    std::ios_base::app);
+	if (!exists(fName.c_str())){
+		std::fstream newF(fName.c_str(), std::ios_base::out);
+		newF.close();
+	}
+
+	fHandle = new std::fstream(fName.c_str(), opMode);
+
+}
+
+
+
+py::list ImageProcessExtension::exaggerate_color_by_order(py::object& pImageFrame, py::object porder) {
 
 	int order = py::extract<int> (porder);
 
-	globals["imageFrame"] = pImageFrame;
-
-	py::exec("imageFrame = list(imageFrame)", globals);
-
-	py::list imageFrame (globals["imageFrame"]);
+	py::list imageFrame =  convert_image_frame_to_list(pImageFrame);
 
 	uint64_t ySize = py::len(imageFrame);
 	uint64_t xSize = py::len(imageFrame[0]);
@@ -149,8 +311,17 @@ BOOST_PYTHON_MODULE(ImageProcessExtension)
 	enable_numpy_scalar_converter <uint8_t, NPY_UBYTE>();
 
 	py::class_<ImageProcessExtension> ("ImageProcessExtension")
-			.def("exaggerateColorByOrder", &ImageProcessExtension::exaggerateColorByOrder)
-	;
+			.def_readonly("READ",                 &ImageProcessExtension::_READ_                             )
+			.def_readonly("WRITE",                &ImageProcessExtension::_WRITE_                            )
+			.def_readonly("APPEND",       		  &ImageProcessExtension::_APPEND_                           )
+			.def_readonly("DYNAMIC_BIND", 		  &ImageProcessExtension::_DYNAMIC_BIND_                     )
+			.def_readonly("INSTANT_BIND", 		  &ImageProcessExtension::_INSTANT_BIND                      )
+			.def("exaggerateColorByOrder",		  &ImageProcessExtension::exaggerate_color_by_order          )
+			.def("connectToFile", 		  		  &ImageProcessExtension::connect_to_file                    )
+			.def("forceFlushToBuffer",    		  &ImageProcessExtension::force_flush_file_buffer            )
+			.def("writeBinaryImageClipToCSVFile", &ImageProcessExtension::write_binary_image_clip_to_CSV_file)
+			.def("closeConnection",               &ImageProcessExtension::close_connection)
+		;
 
 	py::dict  __globals (py::borrowed(PyEval_GetGlobals()));
 
--- src_cpp_dependence/imageprocutil.h
+++ src_cpp_dependence/imageprocutil.h
@@ -8,16 +8,72 @@
 #ifndef IMAGEPROCUTIL_H_
 #define IMAGEPROCUTIL_H_
 
+#include <fstream>
+#include <deque>
+#include <stdint.h>
+#include <string>
+
 #include <boost/python.hpp>
 #include <Python.h>
+
 namespace py = boost::python;
 
 struct ImageProcessExtension {
 
-	py::list exaggerateColorByOrder (py::object& pImageFrame, py::object porder);
+
+	int openMode;
+
+	int bindType;
+
+	std::fstream *fHandle;
+
+	static const int _READ_  ;
+	static const int _WRITE_ ;
+	static const int _APPEND_;
 
 
+	static const int _DYNAMIC_BIND_;
+	static const int _INSTANT_BIND;
 
+
+	static const int _MAX_THRESHOLD_DYNAMIC_;
+
+	ImageProcessExtension ();
+
+	~ImageProcessExtension();
+
+
+	void force_flush_file_buffer ();
+
+	py::list exaggerate_color_by_order (py::object& pImageFrame, py::object porder);
+
+	void connect_to_file (py::object& pFName, py::object& pOpenMode,
+			py::object& pBindType);
+
+	void close_connection ();
+
+	void write_binary_image_clip_to_CSV_file (py::object& pImageFrame);
+
+	py::list read_binary_image_clip_from_CSV_file (py::tuple& pos1,
+			py::tuple& pos2);
+
+private:
+
+	py::list convert_image_frame_to_list (py::object& pImageFrame);
+
+	std::deque <std::string> fileBuffer;
+
+	static const std::string __one;
+	static const std::string __zero;
+	static const std::string __comma;
+	static const std::string __nextline;
+
+	std::string itoa (int num);
+
+
+	void dynamicBindData ();
+
+	int exists(const char *fname);
 };
 
 
